a97d7e7b16c0f240d4e0f82352256d426a19da21
joiicebatista <joiicebatista123@gmail.com>
2025-08-22 13:12:07 -0300

    feat: add css transition

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -1,48 +1,74 @@
+import "./AddTaskDialog.css"
+
+import { useRef } from "react"
 import { createPortal } from "react-dom"
+import { CSSTransition } from "react-transition-group"
 
 import Button from "./Button"
 import Input from "./Input"
 
 const AddTaskDialog = ({ isOpen, handleClose }) => {
-  if (!isOpen) return null
-  return createPortal(
-    <div className="fixed bottom-0 left-0 top-0 flex h-screen w-screen items-center justify-center bg-[#09090B1F]">
-      <div className="gap-4 rounded-xl bg-[#FFFFFF] p-5 text-center">
-        {/* <div className="items-center gap-4"> */}
-        <h2 className="text-xl font-semibold text-[#35383E]">Nova Tarefa</h2>
-        <p className="mb-4 mt-1 text-sm font-normal text-[#9A9C9F]">
-          Insira as informações abaixo
-        </p>
-        <div className="flex w-[336px] flex-col space-y-4">
-          <Input
-            id="title"
-            label="Título"
-            placeholder="Insira o título da tarefa"
-          ></Input>
-          <Input id="time" label="Horário" placeholder="Selecione"></Input>
-          <Input
-            id="description"
-            label="Descrição"
-            placeholder="Descreva a tarefa"
-          ></Input>
-          <div className="flex gap-3">
-            <Button
-              size="large"
-              variant="secondary"
-              className="w-full"
-              onClick={handleClose}
-            >
-              Cancelar
-            </Button>
-            <Button size="large" className="w-full">
-              Salvar
-            </Button>
-          </div>
-        </div>
+  const nodeRef = useRef()
+
+  return (
+    <CSSTransition
+      nodeRef={nodeRef}
+      in={isOpen}
+      timeout={500}
+      classNames="add-task-dialog"
+      unmountOnExit
+    >
+      <div>
+        {createPortal(
+          <div
+            ref={nodeRef}
+            className="fixed bottom-0 left-0 top-0 flex h-screen w-screen items-center justify-center bg-[#09090B1F]"
+          >
+            <div className="gap-4 rounded-xl bg-[#FFFFFF] p-5 text-center">
+              {/* <div className="items-center gap-4"> */}
+              <h2 className="text-xl font-semibold text-[#35383E]">
+                Nova Tarefa
+              </h2>
+              <p className="mb-4 mt-1 text-sm font-normal text-[#9A9C9F]">
+                Insira as informações abaixo
+              </p>
+              <div className="flex w-[336px] flex-col space-y-4">
+                <Input
+                  id="title"
+                  label="Título"
+                  placeholder="Insira o título da tarefa"
+                ></Input>
+                <Input
+                  id="time"
+                  label="Horário"
+                  placeholder="Selecione"
+                ></Input>
+                <Input
+                  id="description"
+                  label="Descrição"
+                  placeholder="Descreva a tarefa"
+                ></Input>
+                <div className="flex gap-3">
+                  <Button
+                    size="large"
+                    variant="secondary"
+                    className="w-full"
+                    onClick={handleClose}
+                  >
+                    Cancelar
+                  </Button>
+                  <Button size="large" className="w-full">
+                    Salvar
+                  </Button>
+                </div>
+              </div>
+            </div>
+            {/* </div> */}
+          </div>,
+          document.body
+        )}
       </div>
-      {/* </div> */}
-    </div>,
-    document.body
+    </CSSTransition>
   )
 }
 
