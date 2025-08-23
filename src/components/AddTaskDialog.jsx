7b7e564adfef0aa95024c985f42b2641479e575a
joiicebatista <joiicebatista123@gmail.com>
2025-08-23 12:12:49 -0300

    feat: ensure error when the inputs are blank

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -23,6 +23,9 @@ const AddTaskDialog = ({ isOpen, handleClose, handleSubmit }) => {
   }, [isOpen])
 
   const handleSaveClick = () => {
+    if (!title.trim() || !description.trim()) {
+      return alert("Preencha todos os campos")
+    }
     handleSubmit({
       id: v4(),
       title,
